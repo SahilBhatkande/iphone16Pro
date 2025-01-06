661dfe158f2507aa631244f70fd1a4693a095d82
sahil07-git <sahilbhatkande231@gmail.com>
2025-01-06 23:19:11 +0530

    Fixed import path for animations

--- eslint.config.js
+++ eslint.config.js
@@ -3,6 +3,7 @@ import globals from 'globals'
 import react from 'eslint-plugin-react'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
+import { ESLint } from 'eslint'
 
 export default [
   { ignores: ['dist'] },
@@ -22,6 +23,9 @@ export default [
       react,
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
+    },
+    ESLint:{
+        ignoreDuringBuilds : true,
     },
     rules: {
       ...js.configs.recommended.rules,
--- src/Components/Features.jsx
+++ src/Components/Features.jsx
@@ -1,10 +1,12 @@
 import { useGSAP } from '@gsap/react'
 import React, { useRef } from 'react'
-import { animateWithGsap } from '../utils/animations';
+import { animateWithGsap } from '../utils/Animations';
 import { explore1Img, explore2Img, exploreVideo } from '../utils';
 import gsap from 'gsap';
+import animations from '../utils/Animations';
 
-const Features = () => {
+
+const  Features = () => {
   const videoRef = useRef();
 
   useGSAP(() => {
